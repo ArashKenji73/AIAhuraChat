bc675cd0ae45c6f9b21a58b0da57d637b3064186
ArashKenji73 <kenjiarash@gmail.com>
2025-11-22 10:06:36 +0330

    fix: placeholder color on dark theme

--- src/components/chat/ChatInput.vue
+++ src/components/chat/ChatInput.vue
@@ -1,7 +1,7 @@
 <template>
 <footer class="p-3 border-t border-slate-200">
 <form @submit.prevent="send" class="flex gap-2 items-end">
-<textarea v-model="text" placeholder="پیام خود را بنویسید..." class="w-full p-3 rounded-lg border border-slate-300"></textarea>
+<textarea v-model="text" placeholder="پیام خود را بنویسید..." class="w-full p-3 rounded-lg border border-slate-300 dark:text-white dark:placeholder-gray-400"></textarea>
 <button class="px-4 py-2 bg-indigo-500 text-white rounded-lg">ارسال</button>
 </form>
 </footer>
